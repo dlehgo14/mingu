5082b312f4d8b7fa7743cc36a4c704746aa195cf
DESKTOP-7DVLGQ9\my <dlehgo14@likelion.org>
2018-07-13 13:02:42 +0900

    edit

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -35,7 +35,7 @@ class KakaoController < ApplicationController
 		elsif @response == "나이"
 			@msg = {
 				message: {
-					text: "저는 20대에요..!\n\n\n\n....여자 나이를 물어보는건 반칙이라구!! 그렇게 많지 않아!!"
+					text: "저는 20대에요..!\n\n\n\n....여자 나이를 물어보는건 반칙이라구!! 그렇게 많지 않아!!",
 					photo: {
 					    url: "https://s3.ap-northeast-2.amazonaws.com/tongilstorage/kakaochat/3.PNG",
 					    width: 640,
@@ -51,7 +51,7 @@ class KakaoController < ApplicationController
 		elsif @response == "성격"
 			@msg = {
 				message: {
-					text: "제 성격은 밝고 쾌활해요!! 모든 일이든 긍정적으로 생각하고 둥글둥글하답니다!! \n 하지만 항상 그런건 아니에요.. 가끔 화가 날때면 정말 무섭다구요.. 그러니까 상처주지 말아요!!!"
+					text: "제 성격은 밝고 쾌활해요!! 모든 일이든 긍정적으로 생각하고 둥글둥글하답니다!! \n 하지만 항상 그런건 아니에요.. 가끔 화가 날때면 정말 무섭다구요.. 그러니까 상처주지 말아요!!!",
 					photo: {
 					    url: "https://s3.ap-northeast-2.amazonaws.com/tongilstorage/kakaochat/3.PNG",
 					    width: 640,
@@ -67,7 +67,7 @@ class KakaoController < ApplicationController
 		elsif @response == "특징"
 			@msg = {
 				message: {
-					text: "허허.. 자네 나의 특징이 궁금한가... 그렇다면.. 나의 인스타에 놀러오게나"
+					text: "허허.. 자네 나의 특징이 궁금한가... 그렇다면.. 나의 인스타에 놀러오게나",
 					photo: {
 					    url: "https://s3.ap-northeast-2.amazonaws.com/tongilstorage/kakaochat/3.PNG",
 					    width: 640,
@@ -87,7 +87,7 @@ class KakaoController < ApplicationController
 		elsif @response == "좋아하는 것"
 			@msg = {
 				message: {
-					text: "저는 고구마를 좋아해요! 그리고 브로콜리랑 양배추도 좋아해요.... 사실은 닭발이 먹고싶어요... 찜닭도 먹고싶고 낙곱새도 먹고싶어요... 다이어트 너무 힘들어요ㅜㅜ"
+					text: "저는 고구마를 좋아해요! 그리고 브로콜리랑 양배추도 좋아해요.... 사실은 닭발이 먹고싶어요... 찜닭도 먹고싶고 낙곱새도 먹고싶어요... 다이어트 너무 힘들어요ㅜㅜ",
 					photo: {
 					    url: "https://s3.ap-northeast-2.amazonaws.com/tongilstorage/kakaochat/3.PNG",
 					    width: 640,
@@ -122,7 +122,7 @@ class KakaoController < ApplicationController
 				}
 			}
 			render json: @msg, status: :ok
-		elsif @response == "아니요" 
+		elsif @response == "아니요" ||@response == "아무것도 아니에요" ||@response == "아..그렇군요"
 			@msg = {
 				message: {
 					text: "싱겁네요..."
